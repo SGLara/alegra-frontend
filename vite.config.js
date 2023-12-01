b00535cbbd6c31426ce43c0a3866af44be8ad8ce
Steven Lara <steven.lara@elaniin.com>
2023-11-30 18:23:42 -0600

    :wrench: Update configs

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,12 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  watch: {
+    usePolling: true
+  },
   server: {
-    hos: true,
+    host: true,
     strictPort: true,
-    port: 5173
+    port: 4173
   }
 })
